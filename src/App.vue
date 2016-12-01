a0d8d566bb53adab147219aee6dcf733879ca627
Hbinbin <haobin@haobindeMacBook-Pro.local>
2016-12-01 14:31:10 +0800

    vue-modal示例

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+      <h1>.vue组件方式的模态框</h1>
+      <button @click="showModal">模态框</button>
+      <modal-child :msg="myModal" @close="closeModal"><!--将myModal的值传给绑定的属性msg--><!--close监听子组件触发的事件-->
+          <h2 slot="header">header</h2>
+          <h3 slot="body">body</h3>
+          <h3 slot="footer">footer</h3>
+      </modal-child>
+  </div>
+</template>
+
+<script>
+import ModalChild from 'components/ModalChild'
+
+export default {
+  name: 'app',
+  components: {
+     ModalChild
+  },
+  data () {
+     return {
+         myModal:false
+     }
+  },
+  watch:{
+     myModal:{
+        handler:function(val,oldVal){
+           console.log('myModal的值变化为'+val)
+        },
+        deep:true
+     }
+  },
+  methods:{
+     showModal:function(){
+         if(this.myModal){
+             this.myModal=false;
+             console.log('关闭模态框');
+         }else{
+             this.myModal=true;
+             console.log('打开模态框');
+         }
+     },
+     closeModal:function(){//定义close的事件函数
+         this.myModal=false;
+     }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ModalChild2.vue
@@ -0,0 +1,110 @@
+<!--button在组件内-->
+<template>
+    <div class="modal">
+        <button @click="showModal">模态框</button>
+        <transition name="modal">
+            <div class="modal-mask" v-show="showContainer">
+                <div class="modal-wrapper">
+                    <transition
+                            name="modal-container"
+                            enter-active-class="animated rubberBand"
+                            leave-active-class="animated hinge"
+                    >
+                        <div class="modal-container" v-show="showContainer">
+                            <div class="modal-header">
+                                <h3 name="header">header</h3>
+                            </div>
+                            <div class="modal-body">
+                                <h3 name="body">body</h3>
+                            </div>
+                            <div class="modal-footer">
+                                <h3 name="footer">footer</h3>
+                                <button class="modal-default-button" @click.stop="showModal">close</button>
+                            </div>
+                        </div>
+                    </transition>
+                </div>
+            </div>
+        </transition>
+    </div>
+</template>
+<style>
+
+    .modal-mask {
+            position: fixed;
+            z-index: 9998;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0, 0, 0, .3);
+            display: table;
+    }
+
+    .modal-wrapper {
+        display: table-cell;
+        vertical-align: middle;
+    }
+
+    .modal-container {
+        width: 300px;
+        margin: 0px auto;
+        padding: 20px 30px;
+        background-color: #fff;
+        border-radius: 2px;
+        box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
+        font-family: Helvetica, Arial, sans-serif;
+    }
+
+    .modal-header h3 {
+        margin-top: 0;
+        color: #42b983;
+    }
+
+    .modal-body {
+        margin: 20px 0;
+    }
+
+    .modal-default-button {
+        float: right;
+    }
+
+    .modal-enter,.modal-leave-active{
+       opacity: 0;
+    }
+    .modal-enter-active{
+        /*opacity: 1;*/
+        transition:opacity .5s ease;
+    }
+    .modal-leave-active{
+        opacity: 0;
+        transition:opacity 1s linear 2s;
+    }
+    @import "../assets/css/animate.css";/*引入animate.css库*/
+</style>
+<script>
+    export default{
+        name:'modal',
+        data () {
+            return{
+                showContainer:false
+            }
+        },
+        methods:{
+            showModal:function(){
+                if(this.showContainer){
+                    this.showContainer=false;
+                }else{
+                    this.showContainer=true;
+                }
+            },
+            showModal1:function(){
+                if(this.showContainer){
+                    this.showContainer=false;
+                }else{
+                    this.showContainer=true;
+                }
+            }
+        }
+    }
+</script>
